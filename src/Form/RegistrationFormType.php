4ddd3e02fcad96151043180638029ecd1759bf90
luboys <luboys>
2023-09-19 17:07:30 +0200

    testtf

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -8,44 +8,84 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
+
+
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
+        ->add('name', TextType::class, [
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Entrez votre prénom.',
+                ]),
+                new Length([
+                    'min' => 2,
+                    'max' => 255,
+                    'minMessage' => 'Your name should be at least {{ limit }} characters',
+                    'maxMessage' => 'Your name should not be longer than {{ limit }} characters',
+                ]),
+            ],
+        ])
+        ->add('nickname', TextType::class, [
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Entrez votre nom.',
+                ]),
+                new Length([
+                    'min' => 2,
+                    'max' => 255,
+                    'minMessage' => 'Your nickname should be at least {{ limit }} characters',
+                    'maxMessage' => 'Your nickname should not be longer than {{ limit }} characters',
+                ]),
+            ],
+        ])
+        ->add('username', TextType::class, [
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Entré votre pseudo.',
+                ]),
+                new Length([
+                    'min' => 2,
+                    'max' => 255,
+                    'minMessage' => 'Your nickname should be at least {{ limit }} characters',
+                    'maxMessage' => 'Your nickname should not be longer than {{ limit }} characters',
+                ]),
+            ],
+        ])
+        ->add('email')
+        ->add('agreeTerms', CheckboxType::class, [
+            'mapped' => false,
+            'constraints' => [
+                new IsTrue([
+                    'message' => 'You should agree to our terms.',
+                ]),
+            ],
+        ])
+        ->add('plainPassword', PasswordType::class, [
+            'mapped' => false,
+            'attr' => ['autocomplete' => 'new-password'],
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Please enter a password',
+                ]),
+                new Length([
+                    'min' => 6,
+                    'minMessage' => 'Your password should be at least {{ limit }} characters',
+                    'max' => 4096,
+                ]),
+            ],
+        ]);
+    
     }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
